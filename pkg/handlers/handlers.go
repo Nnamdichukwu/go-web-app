6fd2882acabde4ebc1baa58e99efeb08d0609f3b
Nnamdichukwu <nnamdi.victor.chibueze@gmail.com>
2024-02-02 21:37:22 +0000

    creating base layout

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -4,11 +4,11 @@ import (
 	"github.com/Nnamdichukwu/go-web-app/pkg/renders"
 )
 func Home(w http.ResponseWriter, r *http.Request){
-	renders.RenderTemplate(w, "home.page.html")
+	renders.RenderTemplate(w, "home.page.gohtml")
 }
 
 func About(w http.ResponseWriter, r *http.Request){
-	renders.RenderTemplate(w, "about.page.html")
+	renders.RenderTemplate(w, "about.page.gohtml")
 	
 }
 
--- pkg/renders/renders.go
+++ pkg/renders/renders.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 func RenderTemplate(w http.ResponseWriter, tmpl string){
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl,"./templates/base.layout.gohtml")
 	if err := parsedTemplate.Execute(w, nil);  err != nil{
 		fmt.Println("error parsing template:", err)
 		return
