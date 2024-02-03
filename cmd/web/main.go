855c3bfa5799b619f9cd0748c13f1b766eafdb29
Nnamdichukwu <nnamdi.victor.chibueze@gmail.com>
2024-02-03 21:54:51 +0000

    create app config to hold template cache

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/Nnamdichukwu/go-web-app/pkg/config"
 	"github.com/Nnamdichukwu/go-web-app/pkg/handlers"
-	
+	"github.com/Nnamdichukwu/go-web-app/pkg/renders"
 )
 const portNumber = ":8080"
 
 func main()  {
-	
+	var app config.AppConfig
+
+	tc, err := renders.CreateTemplateCache()
+	if err != nil{
+		log.Fatal("cannot create template cache")
+	}
+	app.TemplateCache = tc 
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about",handlers.About)
 
--- pkg/renders/renders.go
+++ pkg/renders/renders.go
@@ -1,50 +1,66 @@
 package renders
 
 import (
-	"fmt"
+	"bytes"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
-func RenderTemplateTest(w http.ResponseWriter, tmpl string){
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl,"./templates/base.layout.gohtml")
-	if err := parsedTemplate.Execute(w, nil);  err != nil{
-		fmt.Println("error parsing template:", err)
-		return
-	}
-}
 
-var tc = make(map[string]*template.Template)
-func RenderTemplate(w http.ResponseWriter, t string){
-	var tmpl *template.Template
-	var err error
-	_, inMap := tc[t]
-	if !inMap{
-		log.Println("creatong a new cache")
-		err = createTemplateCache(t)
-		if err != nil{
-			log.Println(err)
-		}
-	}else {
-		log.Println("using cached template")
+
+func RenderTemplate(w http.ResponseWriter, tmpl string){
+	//get the template cache from the app config
+	tc, err := CreateTemplateCache()
+
+	if err != nil {
+		log.Fatal(err)
 	}
-	tmpl = tc[t]
-	err =  tmpl.Execute(w, nil)
-	if err != nil{
+	t,ok := tc[tmpl]
+
+	if !ok{
+		log.Fatal()
+	}
+	buf := new(bytes.Buffer)
+	err = t.Execute(buf, nil)
+	if err != nil {
 		log.Println(err)
 	}
-	
+	//render the template
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
-func createTemplateCache(t string) error {
-	templates := []string {
-		fmt.Sprintf("./templates/%s",t),
-		"./templates/base.layout.gohtml",
-	}
-	tmpl, err := template.ParseFiles(templates...)
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	//get all of the files name *.page.gohtml from ./templates
+	pages, err := filepath.Glob("./templates/*.page.gohtml")
 	if err != nil{
-		return err
+		return myCache, err
 	}
-	tc[t] = tmpl
-	return nil
+	//range through all files ending with *.page.gohtml
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+
+		if err != nil{
+			return myCache, err
+		}
+		matches, err := filepath.Glob("./templates/*.layout.gohtml")
+		if err != nil{
+			return myCache, err
+		}
+		if len(matches) > 0{
+			ts, err = ts.ParseGlob("./templates/*.layout.gohtml")
+			if err != nil {
+				return myCache, err
+			}
+
+		}
+		myCache[name] = ts
+	}
+	return myCache, nil
 }
