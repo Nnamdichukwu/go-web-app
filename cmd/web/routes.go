7a3867ff655afca18d2b6891782bc44978b84757
Nnamdichukwu <nnamdi.victor.chibueze@gmail.com>
2024-02-07 22:50:23 +0000

    create static images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,6 +17,9 @@ func routes(app *config.AppConfig) http.Handler{
 	mux.Use(SessionLoad)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	//to allow Go load static files
+	fileserver := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileserver))
 	return mux
 
 }
